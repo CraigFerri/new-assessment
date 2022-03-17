10bf9088344cc85a74933237b0a95c9b8cdf0ea7
Craig Ferri <Craig@eyccompanies.com>
2022-03-16 20:53:49 -0400

    The finished product

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // Obtain the img element, and assign it to the image variable
-let image
+let image = document.getElementById('pet')
 
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
-let changeSize
+let changeSize = document.getElementById('shrink-grow')
+var clicks = 0;
 
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+  if (clicks==5){location.href='haha.html'};
+};
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
-  if (image.style.height == "300px") {
+
+
+if (image.style.height != "150px") {
     image.style.height = "150px"
-    image.style.width = "150px"
+    image.style.width = 'auto'
+  
   }
   else {
     image.style.height = "300px"
-    image.style.width = "300px"
+    image.style.width = 'auto'
+
   }
 })
